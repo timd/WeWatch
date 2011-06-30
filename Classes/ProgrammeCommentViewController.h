eb1ab310fd531a5256a6566a30c74b82e1c84baa
Tim Duckett <tim@timduckett.co.uk>
2011-06-30 18:50:35 +0100

    Added new blank view controllers

---
+++ Classes/ProgrammeCommentViewController.h
@@ -0,0 +1,16 @@
+//
+//  ProgrammeCommentViewController.h
+//  WeWatch
+//
+//  Created by Tim Duckett on 30/06/2011.
+//  Copyright 2011 Charismatic Megafauna Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ProgrammeCommentViewController : UIViewController {
+    
+}
+
+@end
---
+++ Classes/ProgrammeTabViewController.h
@@ -0,0 +1,16 @@
+//
+//  ProgrammeTabViewController.h
+//  WeWatch
+//
+//  Created by Tim Duckett on 30/06/2011.
+//  Copyright 2011 Charismatic Megafauna Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ProgrammeTabViewController : UIViewController {
+    
+}
+
+@end
